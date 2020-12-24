6f1adc1b93ebe02bfbacdd9310ceeb62d9f24fb9
Martín López Paglione <martincholp@hotmail.com>
2020-12-24 03:20:09 -0300

    Todos los comandos listos

--- gtcCore.py
+++ gtcCore.py
@@ -9,13 +9,18 @@ class Gcode(object):
 		super(Gcode, self).__init__()
 		self.archivo = archivo
 		self.__velocidadActual = 0.0
-		self.__X = 0.0
-		self.__Y = 0.0
-		self.__Z = 0.0
+		self.__X = None # 0.0
+		self.__Y = None # 0.0
+		self.__Z = None # 0.0
+		self.__E = None # 0.0  # Posicion del filamento
 		self.__materialEmpleado = 0.0
-		self.__longitudTotal = 0.0
+		self.__retraccion = 0.0  # Si hay una retraccion ,primero hay que compensarla antes de sumar material como empleado
+		self.__modoExtrusorRelativo = False  # El eje extrusor tiene coordenadas relativas sin importar el modo de los otros ejes
+		self.__modoRelativo = False  # Todos los ejes son relativos. 
+		self.__distanciaTotalRecorrida = 0.0
 		self.__tiempoTotal = 0.0
 		self.__lineasProcesadas = 0
+		self.__sistemaMetrico = True  # Establece mm, mm/s y mm/s2 cuando es True. Si es False las unidades son pulg, pulg/s, y pulg/s2
 
 	@property
 	def __posicionActual(self):
@@ -58,6 +63,17 @@ class Gcode(object):
 
 		return self.__posicionActual
 
+	def setModoExtrusor(self, relativo):
+		''' Define el modo de movimiento del extrusor'''
+		self.__modoExtrusorRelativo = relativo
+		return relativo
+
+	def setModoPosicionamiento(self, relativo):
+		''' Define el modo de movimiento de todos los ejes'''
+		self.__modoRelativo = relativo
+		return relativo
+
+
 	def setVelocidad(self, vel):
 		"""Establece vel como la velocidad actual"""
 
@@ -75,20 +91,35 @@ class Gcode(object):
 
 		return self.__tiempoTotal
 
-	def updateLongitudTotal(self, dist):
-		"""Aumenta la longitud total la cantidad indicada en dist"""
+	def updateDistanciaTotal(self, dist):
+		"""Aumenta la distancia total la cantidad indicada en dist"""
 
-		self.__longitudTotal = self.__longitudTotal + dist
+		self.__distanciaTotalRecorrida = self.__distanciaTotalRecorrida + dist
 
-	def getLongitudTotal(self):
-		"""Devuelve la longitud total calculada"""
+	def getDistanciaTotal(self):
+		"""Devuelve la distancia total calculada"""
 
-		return self.__longitudTotal
+		return self.__distanciaTotalRecorrida
 
-	def updateMaterialEmpleado(self, dist):
+	def updateMaterialEmpleado(self, val):
 		"""Actualiza la cantidad de material empleado"""
 
-		self.__materialEmpleado = self.__materialEmpleado + dist
+		if not self.__modoExtrusorRelativo:  # Si val no es un valor relativo lo transformo en relativo para el calculo siguiente
+			dist = val - self.__E
+
+
+		# Siendo dist un valor relativo, continuo 
+
+		self.__E = self.__E + dist      # Posicion del material
+
+		cant = dist + self.__retraccion  # Verifico si salio algo de material por el nozzle
+
+		if cant <= 0:
+			self.__retraccion = cant  # Si cantidad es negativo, entonces ese valor es toda la retraccion que tiene el material
+		else:
+			# Si cantidad es positivo, entonces no hay retraccion y ese valor es el material usado
+			self.__retraccion = 0
+			self.__materialEmpleado = self.__materialEmpleado + cant
 
 	def getMaterialEmpleado(self):
 		"""Devuelve la cantidad total de material"""
@@ -134,14 +165,14 @@ class Gcode(object):
 			comando, parametros, comentario = self.parsearLinea(linea)
 			l = l + 1
 
-			if parametros != '':
+			if parametros != '':  # Realizar acciones segun los parametros
 
-				if comando == 'G1':  # Para hacer movimiento
+				if (comando == 'G1') | (comando == 'G0'):  # Para hacer movimiento
 					if 'F' in parametros:  # si hay cambio de velocidad
 						self.setVelocidad(parametros['F'])
 
 					# Punto de destino
-					destino = self.__posicionActual # inicializo
+					destino = self.__posicionActual # inicializo con la pos actual por si alguno de los ejes no hay que modificarlo
 
 					# Cargo la posicion nueva
 					if 'X' in parametros:  
@@ -154,7 +185,7 @@ class Gcode(object):
 					# Calculo la distancia 
 					dist = self.calcDistLineal(destino)
 					# Sumo a la distancia total
-					self.updateLongitudTotal(dist)
+					self.updateDistanciaTotal(dist)
 
 					# Calculo el tiempo empleado
 					tpo = self.calcTiempo(dist)
@@ -165,6 +196,84 @@ class Gcode(object):
 					# Actualizo el material empleado
 					if 'E' in parametros:
 						self.updateMaterialEmpleado(parametros['E'])
+
+				if (comando == 'G4') | (comando == 'M0') | (comando == 'M1'):   # Hace una pausa por un tiempo determinado
+					if 'S' in parametros:  # Si estan definidos los segundos, toma importancia este valor sin importar P
+						self.updateTiempoTotal(float(parametros['S']))
+					else if 'P' in parametros:  # Si solo está P, entonces el valor es en milisegundos
+						self.updateTiempoTotal(float(parametros['P'])/1000)
+
+				if comando == 'G28':  # Hace homing
+					home = True # bandera para hacer home
+					homeCompleto = False # Bandera para home en todos los ejes si o si
+
+					if 'O' in parametros:  # Si la posicion es conocida entonces cancela el homing
+						if (self.__X != None) & (self.__Y != None) & (self.__Z != None):
+							home = False
+						else:
+							homeCompleto = True
+
+					if home:
+						if (not ('X' in parametros) & not ('Y' in parametros) & not ('Z' in parametros)) | homeCompleto:
+							self.__X = 0.0
+							self.__Y = 0.0
+							self.__Z = 0.0
+						else:
+							if 'X' in parametros:  
+								self.__X = 0.0
+							if 'Y' in parametros:
+								self.__Y = 0.0
+							if 'Z' in parametros:
+								self.__Z = 0.0
+
+				if comando == 'G92':  # Establece los ejes al valor indicado
+
+					if 'E' in parametros:
+						self.__E == flaot(parametros['E'])
+					if 'X' in parametros:
+						self.__X == flaot(parametros['X'])
+					if 'Y' in parametros:
+						self.__Y == flaot(parametros['Y'])
+					if 'Z' in parametros:
+						self.__Z == flaot(parametros['Z'])
+					
+
+			else:  # Realizar acciones cuando no hay parametros
+
+				if comando == 'G20':  # Pone las unidades en pulgadas
+					
+					self.__sistemaMetrico = False
+
+				if comando == 'G21':  # Pone las unidades en mm
+
+					self.__sistemaMetrico = True
+
+				if comando == 'G28':  # Hace homing completo si o si
+					self.__X = 0.0
+					self.__Y = 0.0
+					self.__Z = 0.0
+
+				if (comando == 'G4') | (comando == 'M400'):  # Termina de hacer los movimientos de la lista de planeados y hace una pausa hasta que el usuario continue
+					
+					pass  # No hago nada xq el tiempo es indeterminado
+
+				if comando == 'M82':  # Pone solo el extrusor en modo absoluto
+
+					self.__modoExtrusorRelativo = False
+					
+				if comando == 'M83':  # Pone solo el extrusor en modo relativo
+					
+					self.__modoExtrusorRelativo = True
+					
+				if comando == 'G90':  # Pone todos los ejes en modo absoluto (inclusive el extrusor)
+					self.__modoExtrusorRelativo = False
+					self.__modoRelativo = False
+
+				if comando == 'G91':  # Pone todos los ejes en modo relativo (inclusive el extrusor)
+					self.__modoExtrusorRelativo = True
+					self.__modoRelativo = True
+
+
 
 		arch.close()
 
@@ -186,6 +295,6 @@ if __name__ == '__main__':
 	a = Gcode(sys.argv[1])
 	a.parsearGcode()
 	print(a.lineasProcesadas(), " lineas procesadas")
-	print("Distancia recorrida [mm]: ", a.getLongitudTotal())
+	print("Distancia recorrida [mm]: ", a.getDistanciaTotal())
 	print("Tiempo empleado [min]: ", a.getTiempoTotal()/60)
 	print("Material utilizado [mm]: ", a.getMaterialEmpleado())
